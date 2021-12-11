e75cb526e4ff818333c085db6de99c05b5de0869
masaki : ) <pianeko.dev@gmail.com>
2021-12-11 20:31:58 +0900

    [update] add comment
    - 03 of leetcode

--- golang/problems/03-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ golang/problems/03-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,21 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func lengthOfLongestSubstring(s string) int {
 	var result int
 	slice := []rune{}
 
+	//loop based on inputs
 	for _, input := range s {
+		//check if the slice contains a value
 		for i, a := range slice {
 			if a == input {
+				//extract the values after the corresponding value
 				slice = slice[i+1:]
 				break
 			}
 		}
+		//add value
 		slice = append(slice, input)
+		//if the length of the slice is greater than the result, set the length
 		if len(slice) > result {
 			result = len(slice)
 		}
